545be4897ab592dbb66eae584a9b3d4479659680
sakibulhasan0 <bb11021@b-jet.miyazaki-u.ac.jp>
2023-12-25 10:52:31 +0900

    modified for multi clumn

--- table_top_header_multi_column_all.py
+++ table_top_header_multi_column_all.py
@@ -1,21 +1,22 @@
-def table_top_header_multi_column_all(soup):
-  table = soup.find('table')
+def table_top_header_multi_column_all(soup, table):
+  # table = soup.find('table')
   if table:
       table['class'] = 'section__table section__table-D'
-  thead = soup.find('thead')
+  thead = table.find('thead')
   if thead:
       thead['class'] = 'section__table-heading'
 
-  for tr in soup.find_all('tr'):
+  for tr in table.find_all('tr'):
       tr['class'] = 'section__table-row'
 
-  for th in soup.find_all('th'):
+  for th in table.find_all('th'):
       th['class'] = 'section__table-head section__table-head--strong section__table-cell--center'
 
-  tbody = soup.find('tbody')
+  tbody = table.find('tbody')
   if tbody:
       tbody['class'] = 'section__table-body'
 
-  for td in soup.find_all('td'):
+  for td in table.find_all('td'):
       td['class'] = 'section__table-data section__table-cell--center'
-      td_string = td.string_s
+      # td_string = td.string_s
+  return table
